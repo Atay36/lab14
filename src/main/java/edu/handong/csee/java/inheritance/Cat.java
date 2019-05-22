95d25df1331dbfc24288d4e1a52bd54cfda33f9a
Yeeun <48059538+Atay36@users.noreply.github.com>
2019-05-22 20:58:11 +0900

    .

--- src/main/java/edu/handong/csee/java/inheritance/Cat.java
+++ src/main/java/edu/handong/csee/java/inheritance/Cat.java
@@ -1,22 +1,22 @@
 package edu.handong.csee.java.inheritance;
 
 public class Cat extends Animal {
-    public static void testClassMethod() {
+    public static void testClassMethod() {  //hiding이다
         System.out.println("The static method in Cat");
     }
-    public void testInstanceMethod() {
+    public void testInstanceMethod() {  //overriding이다
         System.out.println("The instance method in Cat");
     }
 
     public static void main(String[] args) {
         Cat myCat = new Cat();
-        Animal myAnimal = myCat;
-        Animal.testClassMethod();
-        myAnimal.testInstanceMethod();//cat 클래스의 testInstanceMethod 호출한
+        Animal myAnimal = myCat;    // polymorphism이다
+        Animal.testClassMethod();   // animal메소드의클래스를 출력한다
+        myAnimal.testInstanceMethod();// 인스턴스 메소드인 cat을 출력한다 
         
         Dog myDog = new Dog();
-        Animal myAnimal2 = myDog;
-        Animal.testClassMethod();
-        myAnimal2.testInstanceMethod();//dog 클래스의 testInstanceMethod 호출한
+        Animal myAnimal2 = myDog;   //polymorphism이다
+        Animal.testClassMethod();   // animal메소드의클래스를 출력한다
+        myAnimal2.testInstanceMethod();// 인스턴스 메소드인 dog을 출력한다
     }
 }
